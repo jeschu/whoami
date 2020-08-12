df4495a8a37caef6d568e53e696295ef8024b917
Jens Schumacher <jeschu@ok.de>
2020-08-12 23:09:03 +0200

    Cleaner Code

--- whoami.go
+++ whoami.go
@@ -10,12 +10,14 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		//goland:noinspection GoUnhandledErrorResult
 		defer r.Body.Close()
-		lines := make([]string, 0)
+		lines := new(Lines).Init()
 		path, err := url.PathUnescape(r.URL.Path)
 		if err != nil {
 			path = r.URL.Path
@@ -29,29 +31,26 @@ func main() {
 		} else {
 			query = "?" + query
 		}
-		lines = append(lines, fmt.Sprintf("%s %s%s %s (remoteAddr=%s)", r.Method, path, query, r.Proto, r.RemoteAddr))
+		lines.Addf("%s %s%s %s (remoteAddr=%s)", r.Method, path, query, r.Proto, r.RemoteAddr)
 
-		headers := make([]string, 0, len(r.Header))
-		for header, _ := range r.Header {
-			headers = append(headers, header)
+		for _, header := range sortedHeaderKeys(r.Header) {
+			lines.Addf("%s: %v", header, strings.Join(r.Header[header], ", "))
 		}
-
-		sort.Strings(headers)
-		for _, header := range headers {
-			lines = append(lines, fmt.Sprintf("%s: %v", header, strings.Join(r.Header[header], ", ")))
-		}
-		lines = append(lines, "")
+		lines.AddEmpty()
 		if body, err := ioutil.ReadAll(r.Body); err == nil {
-			lines = append(lines, string(body))
+			lines.AddBytes(body)
 		} else {
-			lines = append(lines, fmt.Sprintf("error reading request body: %v", err))
+			lines.Addf("error reading request body: %v", err)
 		}
-		bodyBytes := []byte(strings.Join(lines, "\n"))
+		bodyBytes := lines.Bytes()
+		fmt.Print("[")
+		fmt.Print(time.Now().Format(time.RFC3339Nano))
+		fmt.Print("] ")
 		fmt.Println(string(bodyBytes))
 		w.WriteHeader(200)
 		w.Header().Add("Content-Type", "text/plain")
 		w.Header().Add("Content-Length", strconv.Itoa(len(bodyBytes)))
-		w.Write(bodyBytes)
+		_, _ = w.Write(bodyBytes)
 	})
 	fmt.Println("listen at 0.0.0.0:80")
 	if err := http.ListenAndServe("0.0.0.0:80", nil); err != nil {
@@ -59,3 +58,40 @@ func main() {
 	}
 	os.Exit(0)
 }
+
+func sortedHeaderKeys(header http.Header) []string {
+	var keys = make([]string, 0, len(header))
+	for h := range header {
+		keys = append(keys, h)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+type Lines struct {
+	lines []string
+}
+
+func (lines *Lines) Init() *Lines {
+	lines.lines = make([]string, 0)
+	return lines
+}
+
+func (lines *Lines) Addf(format string, a ...interface{}) *Lines {
+	lines.lines = append(lines.lines, fmt.Sprintf(format, a...))
+	return lines
+}
+
+func (lines *Lines) AddEmpty() *Lines {
+	lines.lines = append(lines.lines, "")
+	return lines
+}
+
+func (lines *Lines) AddBytes(bytes []byte) *Lines {
+	lines.lines = append(lines.lines, string(bytes))
+	return lines
+}
+
+func (lines *Lines) String() string { return strings.Join(lines.lines, "\n") }
+
+func (lines *Lines) Bytes() []byte { return []byte(lines.String()) }
